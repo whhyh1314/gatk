99808a04a88d7948b6afecd2b9e5868cfbdd2077
Takuto Sato <tsato@broadinstitute.org>
2017-07-26 14:57:51 -0400

    create ContextDependentArtifactFilter.java and ContextDependentArtifactFilterEngine.java

---
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/orientationbias/ContextDependentArtifactFilterEngine.java
@@ -0,0 +1,14 @@
+package org.broadinstitute.hellbender.tools.walkers.orientationbias;
+
+import htsjdk.variant.variantcontext.VariantContext;
+import org.broadinstitute.hellbender.engine.FeatureContext;
+import org.broadinstitute.hellbender.engine.ReadsContext;
+import org.broadinstitute.hellbender.engine.ReferenceContext;
+import org.broadinstitute.hellbender.engine.VariantWalker;
+
+/**
+ * Created by tsato on 7/26/17.
+ */
+public class ContextDependentArtifactFilterEngine {
+
+}
