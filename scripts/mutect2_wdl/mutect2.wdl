8dcfa729871d061015711b0106538c6acecc5f3a
lichtens <lichtens@broadinstitute.org>
2017-08-10 16:32:44 -0400

    Eliminating hacky samtools call and using first cut at GetSampleName tool

--- scripts/mutect2_wdl/mutect2.wdl
+++ scripts/mutect2_wdl/mutect2.wdl
@@ -205,10 +205,6 @@ task M2 {
   String? m2_extra_args
 
   command <<<
-  if [[ "_${normal_bam}" == *.bam ]]; then
-      samtools view -H ${normal_bam} | sed -n "/SM:/{s/.*SM:\\(\\)/\\1/; s/\\t.*//p ;q};" > normal_name.txt
-      normal_command_line="-I ${normal_bam} -normal `cat normal_name.txt`"
-  fi
 
   # Use GATK Jar override if specified
   GATK_JAR=${gatk4_jar}
@@ -216,7 +212,16 @@ task M2 {
       GATK_JAR=${gatk4_jar_override}
   fi
 
-  samtools view -H ${tumor_bam} | sed -n "/SM:/{s/.*SM:\\(\\)/\\1/; s/\\t.*//p ;q};" > tumor_name.txt
+  if [[ "_${normal_bam}" == *.bam ]]; then
+      #samtools view -H ${normal_bam} | sed -n "/SM:/{s/.*SM:\\(\\)/\\1/; s/\\t.*//p ;q};" > normal_name.txt
+      java -Xmx4g -jar $GATK_JAR GetSampleName -I ${normal_bam} -O normal_name.txt
+      normal_command_line="-I ${normal_bam} -normal `cat normal_name.txt`"
+  fi
+
+
+
+  #samtools view -H ${tumor_bam} | sed -n "/SM:/{s/.*SM:\\(\\)/\\1/; s/\\t.*//p ;q};" > tumor_name.txt
+  java -Xmx4g -jar $GATK_JAR GetSampleName -I ${tumor_bam} -O tumor_name.txt
 
   java -Xmx4g -jar $GATK_JAR Mutect2 \
     -R ${ref_fasta} \
---
+++ src/main/java/org/broadinstitute/hellbender/tools/GetSampleName.java
@@ -0,0 +1,56 @@
+package org.broadinstitute.hellbender.tools;
+
+import org.broadinstitute.barclay.argparser.Argument;
+import org.broadinstitute.barclay.argparser.BetaFeature;
+import org.broadinstitute.barclay.argparser.CommandLineProgramGroup;
+import org.broadinstitute.barclay.argparser.CommandLineProgramProperties;
+import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
+import org.broadinstitute.hellbender.engine.GATKTool;
+import org.broadinstitute.hellbender.exceptions.UserException;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@CommandLineProgramProperties(
+        summary = "Emit the sample name from the bam header.",
+        oneLineSummary = "If the bam has more than one sample or zero samples, this tool will error, by design.  This tool has not been tested extensively.",
+        programGroup = CommandLineProgramGroup.class
+)
+@BetaFeature
+public class GetSampleName extends GATKTool{
+
+    @Argument(
+            doc = "Output file with only the sample name in it.",
+            fullName = StandardArgumentDefinitions.OUTPUT_LONG_NAME,
+            shortName = StandardArgumentDefinitions.OUTPUT_SHORT_NAME
+    )
+    protected File outputSampleNameFile;
+
+    @Override
+    public void traverse() {
+        // Do nothing!
+    }
+
+    @Override
+    public void onTraversalStart() {
+
+        // Grab the header info
+        if ((getHeaderForReads() == null) || (getHeaderForReads().getReadGroups() == null)) {
+            throw new UserException.BadInput("The given input has no header or no read groups.  Cannot determine a sample name.");
+        }
+
+        final List<String> sampleNames = getHeaderForReads().getReadGroups().stream().map(s -> s.getSample()).distinct().collect(Collectors.toList());
+        if (sampleNames.size() > 1) {
+            throw new UserException.BadInput("The given input has more than one unique sample name.");
+        }
+
+        try (final FileWriter fileWriter = new FileWriter(outputSampleNameFile, false)) {
+            fileWriter.write(sampleNames.get(0));
+        } catch (final IOException ioe) {
+            throw new UserException("Could not write file.", ioe);
+        }
+    }
+}
